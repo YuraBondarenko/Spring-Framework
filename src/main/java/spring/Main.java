e3138b866ad8c301f20488f8bb72ad066c96c40b
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-08 15:52:34 +0200

    created AppConfig and  implemented Main

---
+++ src/main/java/spring/Main.java
@@ -0,0 +1,19 @@
+package spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.config.AppConfig;
+import spring.model.User;
+import spring.service.UserService;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        User firstUser = new User();
+        firstUser.setName("first");
+        firstUser.setAge(12);
+        userService.add(firstUser);
+        userService.listUsers().forEach(System.out::println);
+    }
+}
